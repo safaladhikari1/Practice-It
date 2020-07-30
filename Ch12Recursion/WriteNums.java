7dfabd081f7933c930c3a7482f5ae12166e14e34
Safal Adhikari <safal.adhikari1@outlook.com>
2020-07-29 23:40:47 -0700

    adds problem/solution on WriteNums using recursion

---
+++ Ch12Recursion/WriteNums.java
@@ -0,0 +1,34 @@
+/*
+   Write a method writeNums that accepts an integer parameter n and prints the first n integers starting with 1 in sequential order, separated by commas.
+   
+   Your method should throw an IllegalArgumentException if passed a value less than 1. Note that the output does not advance to the next line.
+*/
+
+public class WriteNums
+{
+   public static void main(String[] args)
+   {
+      //writeNums(5);
+      writeNums(12);
+   }
+   
+   public static void writeNums(int n)
+   {
+      if(n < 1)
+      {
+         throw new IllegalArgumentException();
+      }
+      else
+      {
+         if(n == 1)
+         {
+            System.out.print(n);
+         }
+         else
+         {
+            writeNums(n - 1);
+            System.out.print(", " + n);       
+         }  
+      }  
+   }
+}
